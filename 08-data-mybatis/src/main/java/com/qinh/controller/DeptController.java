38fbf2d5bd164d9effeeed6b531a8569c3ce952b
Qh <771600967@qq.com>
2021-09-14 18:04:28 +0800

    feat:添加mybatis配置测试代码

--- 08-data-mybatis/src/main/java/com/qinh/controller/DeptController.java
+++ 08-data-mybatis/src/main/java/com/qinh/controller/DeptController.java
@@ -1,7 +1,9 @@
 package com.qinh.controller;
 
 import com.qinh.bean.Department;
+import com.qinh.bean.Employee;
 import com.qinh.mapper.DepartmentMapper;
+import com.qinh.mapper.EmployeeMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +21,9 @@ public class DeptController {
     @Autowired
     DepartmentMapper departmentMapper;
 
+    @Autowired
+    EmployeeMapper employeeMapper;
+
     @GetMapping("/dept/{id}")
     public Department getDepartment(@PathVariable("id") Integer id){
         return departmentMapper.getDeptById(id);
@@ -28,6 +33,11 @@ public class DeptController {
     public Department insertDept(Department department){
         departmentMapper.insertDept(department);
         return department;
+    }
+
+    @GetMapping("/emp/{id}")
+    public Employee getEmp(@PathVariable("id") Integer id){
+        return employeeMapper.getEmpById(id);
     }
 
 
